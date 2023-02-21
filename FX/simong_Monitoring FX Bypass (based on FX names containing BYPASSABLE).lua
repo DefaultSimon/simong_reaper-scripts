1a47c9a950382e4ba7b8eb207ac800c3f296fe23
Simon Goričar <simon.peter.goricar@gmail.com>
2023-02-21 18:42:33 +0100

    Monitoring FX scripts: fix script name typo

--- FX/simong_Monitoring FX Bypass (based on FX names containing BYPASSABLE).lua
+++ FX/simong_Monitoring FX Bypass (based on FX names containing BYPASSABLE).lua
@@ -5,12 +5,12 @@
 --   based on their FX name (FX whose name contains "[BYPASSABLE]" are bypassed, 
 --   unbypassed and toggled by these scripts).
 -- @link https://github.com/DefaultSimon/simong_reaper-scripts
--- @version 1.0.6
+-- @version 1.0.7
 -- @changelog
---   - Removed external dependency on the shared library - now included directly with this package instead.
+--   - Fix script name typo.
 -- @metapackage
 -- @provides
 --   [main=main] monitoring-fx-bypass/simong_Bypass all monitoring FX whose names contain BYPASSABLE.lua
---   [main=main] monitoring-fx-bypass/simong_Toggle bypass on all monitoring FX which names containing BYPASSABLE.lua
+--   [main=main] monitoring-fx-bypass/simong_simong_Toggle bypass on all monitoring FX whose names contain BYPASSABLE.lua
 --   [main=main] monitoring-fx-bypass/simong_Unbypass all monitoring FX whose names contain BYPASSABLE.lua
 --   [nomain]    monitoring-fx-bypass/simong_monitoring-fx-bypass-shared-library.lua
